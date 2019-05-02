2f76b40bdb4f5b489a3a1c0d28a7b6f115457fa2
Sidharth Menon <sidharth@startupmission.in>
2019-05-02 17:46:48 +0530

    content edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,8 @@ $(document).ready(function() {
     $(".countdown")
         .countdown("2019/06/01", function(event) {
             $(this).html(
-                event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
+                //event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
+                event.strftime('<div>%D <span>Days</span></div>')
             );
         });
 
